2b802593b0e3d0984967824c4e4ae5c07b22e3f9
Igor Machado <igor.machado@gmail.com>
2018-03-23 15:48:19 -0300

    added opcodes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 #include "Types/ByteArray.hpp"
 #include "BigInteger.hpp"
 
+#include "OpCode.hpp"
+
 using namespace vm;
 using namespace std;
 
 int main() {
+   cout << "Testing..." << endl;
 
+   cout << "75:" << OpCode::PUSHBYTES75 << endl;
 
    cout << "Finished successfully" << endl;
    return 0;
